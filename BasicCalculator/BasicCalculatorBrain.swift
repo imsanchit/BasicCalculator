c81eef0b9d2353291d87bd8a526ce4b56ba0be8d
Sanchit Mittal <sanchitmittal@Sanchits-MacBook-Air.local>
2017-07-27 15:19:48 +0530

    Remove bugs after 1st review

---
+++ BasicCalculator/BasicCalculatorBrain.swift
@@ -0,0 +1,66 @@
+//
+//  BasicCalculatorBrain.swift
+//  BasicCalculator
+//
+//  Created by Sanchit Mittal on 26/07/17.
+//  Copyright © 2017 Sanchit Mittal. All rights reserved.
+//
+
+import Foundation
+
+struct BasicCalculatorBrain {
+    private var accumulator: Double?
+    private var operation: String?
+    
+    mutating func performOperation(_ operand: String , _ symbol: String) {
+    
+    switch symbol {
+        case "+":
+            if let acc = accumulator {
+                accumulator = acc + Double(operand)!
+            }
+            accumulator = Double(operand)
+            operation = symbol
+        case "-":
+            if let acc = accumulator {
+                accumulator = acc - Double(operand)!
+            }
+            accumulator = Double(operand)
+            operation = symbol
+        case "/":
+            if let acc = accumulator {
+                accumulator = acc / Double(operand)!
+            }
+            accumulator = Double(operand)
+            operation = symbol
+        case "*":
+            if let acc = accumulator {
+                accumulator = acc * Double(operand)!
+            }
+            accumulator = Double(operand)
+            operation = symbol
+        case "=":
+            if(operation == "+") {
+                accumulator = accumulator! + Double(operand)!
+                print(accumulator!)
+            }   else if(operation == "-") {
+                accumulator = accumulator! - Double(operand)!
+                print(accumulator!)
+            }  else if(operation == "*") {
+                accumulator = accumulator! * Double(operand)!
+                print(accumulator!)
+            }  else if(operation == "/") {
+                accumulator = accumulator! / Double(operand)!
+                print(accumulator!)
+            }
+            accumulator = Double(round(10000000000000*accumulator!)/10000000000000)
+        default:
+            break
+        }
+    }
+    var result: Double? {
+        get{
+            return accumulator
+        }
+    }
+}
--- BasicCalculator/ViewController.swift
+++ BasicCalculator/ViewController.swift
@@ -10,16 +10,88 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var isUserTyping: Bool = false
+    var basicCalculatorBrain = BasicCalculatorBrain()
+    var firstDecimal: Bool = true
+    @IBOutlet weak var displayScreen: UITextView!
+    
+    @IBAction func performOperations(_ sender: UIButton) {
+        
+        switch sender.currentTitle! {
+            case "+", "-", "*", "/":
+                guard let operand = extractOperandFrom(text: displayScreen.text) else { return }
+                basicCalculatorBrain.performOperation(operand, sender.currentTitle!)
+                displayScreen.text = displayScreen.text! + sender.currentTitle!
+                isUserTyping = false
+                firstDecimal = true
+            
+        case "AC":
+            isUserTyping = false
+            firstDecimal = true
+            displayScreen.text = ""
+        case "<-":
+            if var text = displayScreen.text {
+                if(text != "") {
+                    let index = text.index(before: text.endIndex)
+                    if(text[index]==".") {
+                        firstDecimal = true
+                    }
+                    text = String(text.characters.dropLast())
+                    displayScreen.text = text
+                    if(text.characters.count==0) {
+                        isUserTyping = false
+                    }
+                }
+            }
+        case "=":
+            guard let operand = extractOperandFrom(text: displayScreen.text) else { return }
+            basicCalculatorBrain.performOperation(operand, sender.currentTitle!)
+            
+            if(basicCalculatorBrain.result != nil) {
+                displayScreen.text = String(basicCalculatorBrain.result!)
+                firstDecimal=false
+            }
+            
+        default:
+            break
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func extractOperandFrom(text: String?) -> String? {
+        guard let text = text else { return nil }
+        
+        let endChar = text.characters.last!
+        
+        guard endChar != "+"  &&  endChar != "-"  &&  endChar != "/"  &&  endChar != "*"  &&  endChar != "=" else {
+            return nil
+        }
+        
+        return text
     }
-
-
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        if(sender.currentTitle! == "π") {
+            displayScreen.text = String(Double.pi)
+        }
+        else if(sender.currentTitle! == ".") {
+            if(firstDecimal) {
+                if(!isUserTyping) {
+                    displayScreen.text = sender.currentTitle!
+                    isUserTyping = true
+                    firstDecimal = false
+                    }
+                else {
+                displayScreen.text = displayScreen.text! + sender.currentTitle!
+                firstDecimal = false
+                }
+            }
+        }
+        else if(!isUserTyping) {
+            displayScreen.text = sender.currentTitle!
+            isUserTyping = true
+        }
+        else {
+                displayScreen.text = displayScreen.text! + sender.currentTitle!
+        }
+    }
 }
-
